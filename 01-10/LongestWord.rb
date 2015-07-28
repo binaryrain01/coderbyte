3ee4559cb20d3ed9d3f9b54d2a5744754e2e9329
binaryrain01 <binaryrain01@gmail.com>
2015-07-28 15:34:18 +0900

    Update LongestWord.rb

--- 01-10/LongestWord.rb
+++ 01-10/LongestWord.rb
@@ -1,1 +1,16 @@
+def LongestWord(sen)
 
+  max = 0
+  lword = ''
+  sen.delete! "!@$%^&*():;<>?,./"
+  sen.split.each do |word|
+    if word.length > max
+      max = word.length
+      lword = word
+    end
+  end
+  return lword
+         
+end
+   
+LongestWord(STDIN.gets)
